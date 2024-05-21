54d8ab83d0eaadc7937bdf7e3c7128569fb1ee0d
quangnvo <voquang.usth@gmail.com>
2024-05-21 14:55:25 +0800

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from 'next'
-
+import { Inter } from "next/font/google";
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: 'Vietwander',
-  description: 'Travelling app',
+  description: 'Travelling app for anyone who loves to explore Vietnam.',
 
   // This is the favicon of the website
   // The /favicon.ico is the public path of the favicon
@@ -18,18 +20,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
+    <html lang="en">
+      <body className={inter.className}>
         <Navbar />
-
         <main className='relative overflow-hidden'>
           {children}
         </main>
-
         <Footer />
       </body>
     </html>
